f9b23fa0e246bd3af590cc6f49fb71ca5c014a96
smitakanva <smita.mind2web@gmail.com>
2023-08-22 11:19:18 +0530

    first commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Repositories\Interfaces\CategoryRepositoryInterface;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    private $categoryRepository;
+
+    public function __construct(CategoryRepositoryInterface $categoryRepository){
+        
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function index()
+    {
+        // $data = Category::all();
+        // dd($data);
+       
+        $categories = $this->categoryRepository->allCategories();
+
+        return view('categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'slug' => 'required|string|max:255',
+        ]);
+
+        $this->categoryRepository->storeCategory($data);
+        return redirect()->route('categories.index')->with('message','Category Created Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $category = $this->categoryRepository->findCategory($id);
+
+        return view('categories.edit', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'slug' => 'required|string|max:255',
+        ]);
+
+        $this->categoryRepository->updateCategory($request->all(), $id);
+
+        return redirect()->route('categories.index')->with('message', 'Category Updated Successfully');
+  
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $this->categoryRepository->destroyCategory($id);
+
+        return redirect()->route('categories.index')->with('status', 'Category Delete Successfully');
+    
+    }
+}
